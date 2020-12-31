4c8c1d16021f6ff9c0237224f59f9cd129266ac4
Daniel Reyes Veas <danielreyesveas@gmail.com>
2020-12-31 13:26:25 +0100

    category serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Project, Blog, Job
+from .models import Project, Category, Blog, Job
 
 class ProjectSerializer(serializers.ModelSerializer):
 
@@ -7,7 +7,14 @@ class ProjectSerializer(serializers.ModelSerializer):
         model = Project
         fields = ('pk','title', 'subtitle', 'description', 'image', 'github','url','featured')
 
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
 class BlogSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
 
     class Meta:
         model = Blog
